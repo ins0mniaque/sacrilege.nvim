3ad3861bf9a5ad16c08a1e257c9c28736e181e91
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-01 04:05:09 -0400

    Remove nvim-treesitter-refactor dependency

--- lua/sacrilege/treesitter.lua
+++ lua/sacrilege/treesitter.lua
@@ -1,21 +1,32 @@
+local ts_utils = require("nvim-treesitter.ts_utils")
+local locals = require("nvim-treesitter.locals")
+local utils = require("nvim-treesitter.utils")
+
 local M = { }
 
 function M.definition(opts)
-    require("nvim-treesitter-refactor.navigation").goto_definition(opts and opts.bufnr)
-end
-
-function M.references(opts)
-    local ts_utils = require("nvim-treesitter.ts_utils")
-    local locals = require("nvim-treesitter.locals")
-
     local bufnr = opts and opts.bufnr or vim.api.nvim_get_current_buf()
     local node_at_point = ts_utils.get_node_at_cursor()
+
     if not node_at_point then
         return
     end
 
-    local def_node, scope = locals.find_definition(node_at_point, bufnr)
-    local usages = locals.find_usages(def_node, scope, bufnr)
+    local definition = locals.find_definition(node_at_point, bufnr)
+
+    ts_utils.goto_node(definition)
+end
+
+function M.references(opts)
+    local bufnr = opts and opts.bufnr or vim.api.nvim_get_current_buf()
+    local node_at_point = ts_utils.get_node_at_cursor()
+
+    if not node_at_point then
+        return
+    end
+
+    local definition, scope = locals.find_definition(node_at_point, bufnr)
+    local usages = locals.find_usages(definition, scope, bufnr)
 
     if #usages < 1 then
         return
@@ -39,11 +50,52 @@ function M.references(opts)
     end
 
     vim.fn.setqflist(qf_list, "r")
-    vim.api.nvim_command "copen"
+    vim.api.nvim_command("copen")
 end
 
 function M.rename(new_name, opts)
-    require("nvim-treesitter-refactor.smart_rename").smart_rename(opts and opts.bufnr)
+    local bufnr = opts and opts.bufnr or vim.api.nvim_get_current_buf()
+    local node_at_point = ts_utils.get_node_at_cursor()
+
+    if not node_at_point then
+        utils.print_warning("Nothing to rename")
+        return
+    end
+
+    local function complete_rename(new_name)
+        if not new_name or #new_name < 1 then
+            return
+        end
+
+        local definition, scope = locals.find_definition(node_at_point, bufnr)
+        local nodes_to_rename = { }
+
+        nodes_to_rename[node_at_point:id()] = node_at_point
+        nodes_to_rename[definition:id()] = definition
+
+        for _, n in ipairs(locals.find_usages(definition, scope, bufnr)) do
+            nodes_to_rename[n:id()] = n
+        end
+
+        local edits = { }
+
+        for _, node in pairs(nodes_to_rename) do
+            local lsp_range = ts_utils.node_to_lsp_range(node)
+            local text_edit = { range = lsp_range, newText = new_name }
+            table.insert(edits, text_edit)
+        end
+
+        vim.lsp.util.apply_text_edits(edits, bufnr, "utf-8")
+    end
+
+    if not new_name or #new_name < 1 then
+        local node_text = vim.treesitter.get_node_text(node_at_point, bufnr)
+        local input = { prompt = "New name: ", default = node_text or "" }
+
+        vim.ui.input(input, complete_rename)
+    else
+        complete_rename(new_name)
+    end
 end
 
 return M
