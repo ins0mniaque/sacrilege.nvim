bb840ddd4bea0b5ea46a5109ee9dacf2d1bf9bdc
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-09 04:15:33 -0400

    Add autocomplete module

---
+++ lua/sacrilege/autocomplete.lua
@@ -0,0 +1,74 @@
+local completion = require("sacrilege.completion")
+
+local M = { }
+
+-- TODO: CmdlineChanged auto-complete
+function M.setup()
+    local namespace          = vim.api.nvim_create_namespace("sacrilege/autocomplete")
+    local autocomplete_group = vim.api.nvim_create_augroup("sacrilege/autocomplete", { })
+
+    -- TODO: Add option
+    local function on(char)
+        return char ~= " "
+    end
+
+    local lastrow, lastcol
+    local wasvisible = false
+
+    vim.on_key(function(_, _)
+        wasvisible = completion.visible() ~= nil
+    end, namespace)
+
+    vim.api.nvim_create_autocmd("InsertCharPre",
+    {
+        desc = "Trigger Autocompletion",
+        group = autocomplete_group,
+        callback = function()
+            if vim.fn.state("m") == "m" or completion.visible() then
+                return
+            end
+
+            local char = vim.v.char
+            if #char > 1 then
+                char = char:sub(1, 1)
+            end
+
+            if on(char) then
+                completion.trigger()
+            end
+        end
+    })
+
+    vim.api.nvim_create_autocmd({ "CursorMoved", "TextChangedP" },
+    {
+        desc = "Trigger Autocompletion",
+        group = autocomplete_group,
+        callback = function()
+            lastrow, lastcol = unpack(vim.api.nvim_win_get_cursor(0))
+        end
+    })
+
+    vim.api.nvim_create_autocmd("CursorMovedI",
+    {
+        desc = "Trigger Autocompletion",
+        group = autocomplete_group,
+        callback = function()
+            local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+            if wasvisible and row == lastrow and col < lastcol then
+                local char = col == 0 and " " or vim.api.nvim_buf_get_text(0, row - 1, col - 1, row - 1, col, { })[1]
+
+                if on(char) then
+                    completion.trigger()
+                else
+                    completion.abort()
+                end
+            end
+
+            lastrow = row
+            lastcol = col
+        end
+    })
+end
+
+return M
