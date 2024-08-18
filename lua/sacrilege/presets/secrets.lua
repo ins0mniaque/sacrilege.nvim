9aa78e71f663b939a6583c4688c90e9347bb3216
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-17 22:25:36 -0400

    Add secrets preset

---
+++ lua/sacrilege/presets/secrets.lua
@@ -0,0 +1,14 @@
+local M = { }
+
+-- TODO: Validate options.commands
+function M.apply(options)
+    local command = require("sacrilege.command")
+
+    options.commands.secret = { }
+    options.commands.secret.smile = command.new("Smile", "<Cmd>smile<CR>")
+    options.commands.secret.panic = command.new("Help!", "<C-\\><C-N>:help!<CR>")
+    options.commands.secret.holygrail = command.new("Find Holy Grail", "<Cmd>help holy-grail<CR>")
+    options.commands.secret["42"] = command.new("42", "<Cmd>help 42<CR>")
+end
+
+return M
