7d28f6eb998b5c45616465d8abddf14e420753ce
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-17 21:29:22 -0400

    Fix editor.send function

--- lua/sacrilege/editor.lua
+++ lua/sacrilege/editor.lua
@@ -3,7 +3,7 @@ local localize = require("sacrilege.localizer").localize
 local M = { }
 
 function M.send(keys, remap)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), remap and "t" or "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, true, true), remap and "t" or "n", true)
 end
 
 function M.notify(msg, log_level, opts)
