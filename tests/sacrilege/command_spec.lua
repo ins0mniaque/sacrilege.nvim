04ce328ef9f9a551dc05b609c8c82e30be283a4b
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-21 20:58:45 -0400

    Add command.is tests

--- tests/sacrilege/command_spec.lua
+++ tests/sacrilege/command_spec.lua
@@ -1,11 +1,45 @@
+local command = require("sacrilege.command")
+
 describe("command", function()
-    describe("function", function()
-        it("should succeed", function()
-            assert.are.same(true, true)
+    describe("is", function()
+        it("should return true for commands", function()
+            local cmd = command.new("Test Command")
+
+            assert.are.same(true, command.is(cmd))
         end)
 
-        it("should fail", function()
-            assert.are.same(true, false)
+        it("should return true for linked commands", function()
+            local cmd = command.new("Test Command"):copy()
+
+            assert.are.same(true, command.is(cmd))
+        end)
+
+        it("should return false for commands without metatable", function()
+            local cmd = command.new("Test Command")
+
+            setmetatable(cmd, nil)
+
+            assert.are.same(false, command.is(cmd))
+        end)
+
+        it("should return false for tables", function()
+            assert.are.same(false, command.is({ }))
+        end)
+
+        it("should return false for strings", function()
+            assert.are.same(false, command.is("string"))
+        end)
+
+        it("should return false for numbers", function()
+            assert.are.same(false, command.is(1))
+        end)
+
+        it("should return false for boolean", function()
+            assert.are.same(false, command.is(true))
+        end)
+
+        it("should return false for nil", function()
+            assert.are.same(false, command.is(nil))
         end)
     end)
 end)
