68810010751a21133d6a570b72c6dca1428a8c24
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-22 02:59:23 -0400

    Add command menu sorting

--- lua/sacrilege/ui.lua
+++ lua/sacrilege/ui.lua
@@ -79,14 +79,72 @@ function M.command_palette(buffer)
     M.select(localize("Commands"), commands, function(l, r) return l:lower() < r:lower() end)
 end
 
-function M.command_menu(buffer)
-    local function generate(keymaps)
-        for _, keymap in pairs(keymaps) do
-            if keymap.desc then
-                vim.cmd.amenu("Sacrilege.CommandMenu." .. keymap.desc:gsub(" ", "\\ "):gsub("%.", "\\."):gsub("&", "&&") .. " " .. keymap.lhs)
+local function longest_common_prefix(left, right)
+    local length = 1
+    local prefix = ""
+
+    while length <= #left and length <= #right do
+        local sub = right:sub(1, length)
+        if left:sub(1, length) ~= sub then
+            return prefix
+        end
+        prefix = sub
+        length = length + 1
+    end
+
+    return prefix
+end
+
+local function longest_common_suffix(left, right)
+    local length = 1
+    local suffix = ""
+
+    while length <= #left and length <= #right do
+        local sub = right:sub(-length)
+        if left:sub(-length) ~= sub then
+            return suffix
+        end
+        suffix = sub
+        length = length + 1
+    end
+
+    return suffix
+end
+
+local function remove_affixes(keymaps)
+    local prefix, suffix
+
+    for _, keymap in pairs(keymaps) do
+        if keymap.desc then
+            if not prefix then
+                prefix = keymap.desc
+                suffix = keymap.desc
+            else
+                prefix = longest_common_prefix(prefix, keymap.desc)
+                suffix = longest_common_suffix(suffix, keymap.desc)
             end
         end
     end
+
+    if prefix and prefix:match("[%s%p]$") then
+        for _, keymap in pairs(keymaps) do
+            if keymap.desc then
+                keymap.desc = keymap.desc:sub(#prefix + 1)
+            end
+        end
+    end
+
+    if suffix and suffix:match("^[%s%p]") then
+        for _, keymap in pairs(keymaps) do
+            if keymap.desc then
+                keymap.desc = keymap.desc:sub(1, -#suffix)
+            end
+        end
+    end
+end
+
+function M.command_menu(buffer)
+    local keymaps
 
     local mode = vim.fn.mode()
     if     mode == "s" or mode == "S" or mode == "\19" then mode = "s"
@@ -94,13 +152,33 @@ function M.command_menu(buffer)
     elseif mode ~= "i"                                 then mode = "n"
     end
 
+    if not buffer then
+        keymaps = vim.api.nvim_get_keymap(mode)
+        vim.list_extend(keymaps, vim.api.nvim_buf_get_keymap(0, mode))
+    else
+        keymaps = vim.api.nvim_buf_get_keymap(buffer, mode)
+    end
+
+    keymaps = vim.tbl_filter(function(keymap) return keymap.desc end, keymaps)
+
+    -- TODO: Handle duplicates
+    if #keymaps > 32 then
+        table.sort(keymaps, function(l, r) return #l.desc < #r.desc end)
+        keymaps = vim.list_slice(keymaps, 1, 32)
+    end
+
+    remove_affixes(keymaps)
+
+    table.sort(keymaps, function(l, r) return l.desc:lower() < r.desc:lower() end)
+
     pcall(vim.cmd.aunmenu, "Sacrilege.CommandMenu")
 
-    if not buffer then
-        generate(vim.api.nvim_get_keymap(mode))
-        generate(vim.api.nvim_buf_get_keymap(0, mode))
-    else
-        generate(vim.api.nvim_buf_get_keymap(buffer, mode))
+    if #keymaps == 0 then
+        return false
+    end
+
+    for _, keymap in pairs(keymaps) do
+        vim.cmd.amenu("Sacrilege.CommandMenu." .. keymap.desc:gsub(" ", "\\ "):gsub("%.", "\\."):gsub("&", "&&") .. " " .. keymap.lhs)
     end
 
     return pcall(vim.cmd.popup, "Sacrilege.CommandMenu")
