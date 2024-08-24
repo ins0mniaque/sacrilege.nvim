58c66c3e5e7946a9742e3efb3f7a1d4baedafecc
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-24 16:06:15 -0400

    Refactor autohide window tracking

--- lua/sacrilege/autohide.lua
+++ lua/sacrilege/autohide.lua
@@ -1,36 +1,36 @@
+local localize = require("sacrilege.localizer").localize
+
 local M = { }
 
 local windows
+local lastwindow = 0
 
 local function mousemoved()
-    local mouse = vim.fn.getmousepos()
-    local size  = mouse.winid ~= 0 and windows[mouse.winid]
+    local mouse  = vim.fn.getmousepos()
+    local size   = mouse.winid ~= 0 and windows[mouse.winid]
+    local window = size and mouse.winid or 0
+
+    if window == lastwindow then
+        return
+    end
+
+    lastwindow = window
 
     if size then
-        local width = vim.api.nvim_win_get_width(mouse.winid)
+        local width = vim.api.nvim_win_get_width(window)
         if width == 1 then
-            vim.api.nvim_win_set_width(mouse.winid, size[1])
+            vim.api.nvim_win_set_width(window, size[1])
             return
         end
 
-        local height = vim.api.nvim_win_get_height(mouse.winid)
+        local height = vim.api.nvim_win_get_height(window)
         if height == 1 then
-            vim.api.nvim_win_set_height(mouse.winid, size[2])
+            vim.api.nvim_win_set_height(window, size[2])
             return
         end
-
-        size[1] = width
-        size[2] = height
     else
         for window, size in pairs(windows) do
             if vim.api.nvim_win_is_valid(window) then
-                local width  = vim.api.nvim_win_get_width(window)
-                local height = vim.api.nvim_win_get_height(window)
-                if width ~= 1 and height ~= 1 then
-                    size[1] = width
-                    size[2] = height
-                end
-
                 vim.api.nvim_win_set_width(window, 1)
                 if vim.api.nvim_win_get_width(window) ~= 1 then
                     vim.api.nvim_win_set_height(window, 1)
@@ -52,6 +52,7 @@ local function setup()
     windows = { }
 
     local namespace = vim.api.nvim_create_namespace("sacrilege/autohide")
+    local group     = vim.api.nvim_create_augroup("sacrilege/autohide", { })
 
     vim.o.mousemoveevent = true
 
@@ -60,6 +61,27 @@ local function setup()
             mousemoved()
         end
     end, namespace)
+
+    vim.api.nvim_create_autocmd("WinResized",
+    {
+        desc = localize("Track Window Sizes"),
+        group = group,
+        callback = function(_)
+            for _, window in pairs(vim.v.event.windows) do
+                local size = windows[window]
+
+                if size then
+                    local width  = vim.api.nvim_win_get_width(window)
+                    local height = vim.api.nvim_win_get_height(window)
+
+                    if width ~= 1 and height ~= 1 then
+                        size[1] = width
+                        size[2] = height
+                    end
+                end
+            end
+        end
+    })
 end
 
 function M.disable(window)
