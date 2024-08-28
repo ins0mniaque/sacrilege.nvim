23e526af7d2cc8de708582520afebabd27e0554a
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-28 18:43:36 -0400

    Clean up code

--- lua/sacrilege/presets/secrets.lua
+++ lua/sacrilege/presets/secrets.lua
@@ -4,7 +4,7 @@ local M = { }
 function M.apply(options)
     local command = require("sacrilege.command")
 
-    options.commands.secret = { }
+    options.commands.secret = options.commands.secret or { }
     options.commands.secret.smile = command.new("Smile", "<Cmd>smile<CR>")
     options.commands.secret.panic = command.new("Help!", "<C-\\><C-N>:help!<CR>")
     options.commands.secret.holygrail = command.new("Find Holy Grail", "<Cmd>help holy-grail<CR>")
