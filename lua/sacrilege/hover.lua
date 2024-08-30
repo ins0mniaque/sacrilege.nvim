5b7ba6c645fabf598675a67f9385ef23029cf15d
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-08-29 22:47:02 -0400

    Add hover fold preview

--- lua/sacrilege/hover.lua
+++ lua/sacrilege/hover.lua
@@ -18,6 +18,23 @@ local function diagnostic(mouse)
     _, window = vim.diagnostic.open_float({ pos = { mouse.line - 1, mouse.column - 1 }, relative = "mouse", offset_x = 1 })
 end
 
+local function fold(mouse)
+    local foldstart = vim.fn.foldclosed(mouse.line)
+    local foldend   = vim.fn.foldclosedend(mouse.line)
+    if foldstart == -1 or foldend == -1 then
+        return
+    end
+
+    local lines  = vim.api.nvim_buf_get_lines(0, foldstart - 1, foldend, true)
+    local syntax = vim.bo.syntax == "" and vim.bo.filetype or vim.bo.syntax
+
+    if #lines > 0 then
+        _, window = vim.lsp.util.open_floating_preview(lines, syntax, { relative = "mouse", offset_x = 1 })
+    else
+        close()
+    end
+end
+
 local function hover(mouse)
     local params = vim.lsp.util.make_position_params()
 
@@ -31,21 +48,21 @@ local function hover(mouse)
             return
         end
 
-        local contents, format
+        local contents, syntax
         for _, result in pairs(results) do
             if result.result and result.result.contents then
                 if type(result.result.contents) == 'table' and result.result.contents.kind == 'plaintext' then
-                    format   = 'plaintext'
+                    syntax   = 'plaintext'
                     contents = vim.split(result.result.contents.value or '', '\n', { trimempty = true })
                 else
-                    format   = 'markdown'
+                    syntax   = 'markdown'
                     contents = vim.lsp.util.convert_input_to_markdown_lines(result.result.contents)
                 end
             end
         end
 
         if contents and #contents > 0 then
-            _, window = vim.lsp.util.open_floating_preview(contents, format, { relative = "mouse", offset_x = 1 })
+            _, window = vim.lsp.util.open_floating_preview(contents, syntax, { relative = "mouse", offset_x = 1 })
         else
             close()
         end
@@ -69,6 +86,7 @@ local function mousemoved()
     end
 
     local action = mouse.wincol <= vim.fn.getwininfo(mouse.winid)[1].textoff and diagnostic or
+                   vim.fn.foldclosed(mouse.line) ~= -1                       and fold or
                    editor.supports_lsp_method(0, "textDocument/hover")       and hover or close
 
     vim.defer_fn(function()
